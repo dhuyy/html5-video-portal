2bfe94900b2c0612e02b095a38d5917a5e7918b3
Vandhuy <vandhuy@gmail.com>
2017-10-14 18:57:58 -0300

    #5 Implement Navbar component (topic 2)

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -12,7 +12,13 @@
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
-        controllerAs: 'main'
+        controllerAs: 'vm'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginController',
+        controllerAs: 'vm'
       });
 
     $urlRouterProvider.otherwise('/');
