7fe98f9b982a26c77e921fba79be99edf6048f0b
Vandhuy <vandhuy@gmail.com>
2017-10-15 23:55:40 -0300

    #13 Create Video List on Main Page (Create unit tests for 4*)

--- client/src/app/main/main.spec.js
+++ client/src/app/main/main.spec.js
@@ -5,34 +5,57 @@
    * Unit testing MainController
    */
   describe('[MainController]', function() {
-    var AuthService, $scope, $q, $rootScope, $controller, $state, logoutMethodDeferred, localStorageService;
+    var AuthService, VideoService, $scope, $q, $rootScope, $controller, $state, logoutDeferred, getVideosDeferred,
+      localStorageService;
 
     beforeEach(module('crossoverAssignment'));
-    beforeEach(inject(function(_AuthService_, _$q_, _$rootScope_, _$controller_, _$state_, _localStorageService_) {
+    beforeEach(inject(function(_AuthService_, _VideoService_, _$q_, _$rootScope_, _$controller_, _$state_,
+                               _localStorageService_) {
+
       AuthService = _AuthService_;
+      VideoService = _VideoService_;
       $q = _$q_;
       $rootScope = _$rootScope_;
       $scope = _$rootScope_.$new();
       $state = _$state_;
       localStorageService = _localStorageService_;
 
-      logoutMethodDeferred = $q.defer();
-      spyOn(AuthService, 'logout').and.returnValue(logoutMethodDeferred.promise);
+      logoutDeferred = $q.defer();
+      getVideosDeferred = $q.defer();
+
+      spyOn(AuthService, 'logout').and.returnValue(logoutDeferred.promise);
+      spyOn(VideoService, 'getVideos').and.returnValue(getVideosDeferred.promise);
+
       $controller = _$controller_('MainController', {
         $scope: $scope,
-        AuthService: AuthService
+        AuthService: AuthService,
+        VideoService: VideoService
       })
     }));
+
+    it('should invoke MainController.getVideos when MainController.onInit is invoked', function() {
+      spyOn($controller, 'getVideos');
+
+      $controller.onInit();
+
+      expect($controller.getVideos).toHaveBeenCalled();
+    });
 
     it('should ensure that AuthService.logout is called when MainController.logout is invoked', function() {
       $controller.logout();
       expect(AuthService.logout).toHaveBeenCalled();
     });
 
-    it('should remove sessionId from LocalStorage and redirect to /login when AuthService.logout promise is resolved', function() {
+    it('should ensure that VideoService.getVideos is called when MainController.getVideos is invoked', function() {
+      $controller.getVideos();
+      expect(VideoService.getVideos).toHaveBeenCalled();
+    });
+
+    it('should remove sessionId from LocalStorage and redirect to /login when AuthService.logout promise is resolved',
+      function() {
       $controller.logout();
 
-      logoutMethodDeferred.resolve({
+      logoutDeferred.resolve({
         data: {
           status: 'success'
         }
@@ -42,6 +65,22 @@
 
       expect(localStorageService.get('sessionId')).toEqual(null);
       expect($state.current.url).toEqual('/login');
+    });
+
+    it('should set MainController.videos variable when VideoService.getVideos promise is resolved',
+      function() {
+      $controller.getVideos();
+
+      getVideosDeferred.resolve({
+        data: {
+          status: 'success',
+          data: []
+        }
+      });
+
+      $rootScope.$digest();
+
+      expect($controller.videos).toEqual(jasmine.any(Array));
     });
 
     it('should $on("logout") call vm.logout() when $broadcast("logout") is fired', function() {
